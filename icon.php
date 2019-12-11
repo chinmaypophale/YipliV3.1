fc77361a9eb9bdbaa9111683c70f3fa57365d66b
chinmaypophale <37659306+chinmaypophale@users.noreply.github.com>
2019-12-11 14:05:15 +0530

    Mailer php Extracted

---
+++ icon.php
@@ -0,0 +1,101 @@
+<style>
+#social_side_links {
+	position: fixed;
+  bottom: 40px;
+  left:0;
+  padding:0px;
+  list-style: none;
+  z-index: 99;
+}
+
+#social_side_links li a {display: block;}
+
+#social_side_links li a img {
+	display: block;
+	max-width:100%;
+  padding:7px;
+  -webkit-transition:  background .2s ease-in-out;
+  -moz-transition:  background .2s ease-in-out;
+  -o-transition:  background .2s ease-in-out;
+  transition:  background .2s ease-in-out;
+}
+
+#social_side_links li a:hover img {background: rgba(0, 0, 0, .2);}
+
+
+
+
+</style>
+<ul id="social_side_links">
+	<li><a target="_blank" href="https://api.whatsapp.com/send?phone=919890006839&text=We are interested in Your services. Please Contact Me"><img src="whatsapp.png" width="140" height="110" border="0" alt=""></a></li>
+	
+</ul>
+
+
+
+<style>
+ @media only screen and (min-width:600px) {
+            .hide-on-desktop, * [aria-labelledby='hide-on-desktop'] {
+                display: none;
+                max-height: 0;
+                overflow: hidden;
+            }
+        }
+#social_side_links1 {
+	position: fixed;
+  bottom: 50px;
+  Right: 0;
+  padding:0px;
+  list-style: none;
+  z-index: 99;
+}
+
+#social_side_links1 li a {display: block;}
+
+#social_side_links1 li a img {
+	display: block;
+	max-width:100%;
+  padding:7px;
+  -webkit-transition:  background .2s ease-in-out;
+ animation: shake 0.5s;
+  animation-iteration-count: infinite;
+  -moz-transition:  background .2s ease-in-out;
+  -o-transition:  background .2s ease-in-out;
+  transition:  background .2s ease-in-out;
+}
+@keyframes shake {
+  0% { transform: translate(1px, 1px) rotate(0deg); }
+  10% { transform: translate(-1px, -2px) rotate(-1deg); }
+  20% { transform: translate(-3px, 0px) rotate(1deg); }
+  30% { transform: translate(3px, 2px) rotate(0deg); }
+  40% { transform: translate(1px, -1px) rotate(1deg); }
+  50% { transform: translate(-1px, 2px) rotate(-1deg); }
+  60% { transform: translate(-3px, 1px) rotate(0deg); }
+  70% { transform: translate(3px, 1px) rotate(-1deg); }
+  80% { transform: translate(-1px, -1px) rotate(1deg); }
+  90% { transform: translate(1px, 2px) rotate(0deg); }
+  100% { transform: translate(1px, -2px) rotate(-1deg); }
+}
+
+ #callmeMain {
+    
+    
+    -webkit-animation: zcwmini2 1.5s 0s ease-out infinite;
+    -moz-animation: zcwmini2 1.5s 0s ease-out infinite;
+    animation: zcwmini2 1.5s 0s ease-out infinite;
+}
+
+#social_side_links1 li a:hover img {background: rgba(0, 0, 0, .2);}
+
+
+
+</style>
+<div class="hide-on-desktop" aria-labelledby="hide-on-desktop" style="mso-hide: all;">
+       
+    
+<ul id="social_side_links1">
+	<li><a href="tel:9890006839"><img src="phonecall.png" width="90" height="85" border="0" alt=""><div id="callmeMain"></div></a></li>
+	
+	
+</ul>
+</div>
